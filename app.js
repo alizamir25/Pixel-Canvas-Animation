f78d9dcf7d145e0b29bdeca6cce5394612d86560
alizamir25 <ali0786zamir@gmail.com>
2024-12-07 14:02:04 +0000

    Patch

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-class Pixel {
-    constructor(canvas, context, x, y, color, speed, delay) {
-      this.width = canvas.width;
-      this.height = canvas.height;
-      this.ctx = context;
-      this.x = x;
-      this.y = y;
-      this.color = color;
-      this.speed = this.getRandomValue(0.1, 0.9) * speed;
-      this.size = 0;
-      this.sizeStep = Math.random() * 0.4;
-      this.minSize = 0.5;
-      this.maxSizeInteger = 2;
-      this.maxSize = this.getRandomValue(this.minSize, this.maxSizeInteger);
-      this.delay = delay;
-      this.counter = 0;
-      this.counterStep = Math.random() * 4 + (this.width + this.height) * 0.01;
-      this.isIdle = false;
-      this.isReverse = false;
-      this.isShimmer = false;
+class Pixel{
+    constructor(canvas, context, x, y, color, speed, delay){
+      this.width=canvas.width;
+      this.height=canvas.height;
+      this.ctx=context;
+      this.x=x;
+      this.y=y;
+      this.color=color;
+      this.speed=this.getRandomValue(0.1, 0.9)*speed;
+      this.size=0;
+      this.sizeStep=Math.random()*0.4;
+      this.minSize=0.5;
+      this.maxSizeInteger=2;
+      this.maxSize=this.getRandomValue(this.minSize, this.maxSizeInteger);
+      this.delay=delay;
+      this.counter=0;
+      this.counterStep=Math.random()*4+(this.width+this.height)*0.01;
+      this.isIdle=false;
+      this.isReverse=false;
+      this.isShimmer=false;
     }
   
     getRandomValue(min, max) {
